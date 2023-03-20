778ac81a79c0a48b6f65dbc1d7d9e3e4a283f0da
quyp <1095698022@qq.com>
2023-03-20 18:05:22 +0800

    update

--- src/pages/me/index.vue
+++ src/pages/me/index.vue
@@ -1,54 +1,14 @@
 <template>
-  <view class="content">
-    <image class="logo" src="/static/logo.png" />
-    <view class="text-area">
-      <text class="title">{{ title }}</text>
-    </view>
-    <button
-      class="btn"
-      round
-      size="small"
-      open-type="getPhoneNumber"
-      @getphonenumber="getPhoneNumber"
-    >
-      <i class="icon-wechat" />微信用户一键登录
-    </button>
-  </view>
+  <view class="me"> {{ title }}</view>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
-const title = ref("login");
-const getPhoneNumber = (e) => {
-  const { code } = e.detail;
-  console.log("number", code);
-};
+const title = ref("me");
 </script>
 
 <style>
-.content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
-}
-
-.text-area {
-  display: flex;
-  justify-content: center;
-}
-
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
+.me {
+  text-align: center;
 }
 </style>
